3aa9afb7959f52a84b818775064166f4bdb7225d
angelynz95 <angel_lynz95@hotmail.com>
2017-03-16 02:49:48 +0700

    remove year from courses table

---
+++ Ohoo/database/migrations/2017_03_15_194551_remove_year_from_courses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveYearFromCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->dropColumn('year');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->integer('year')->unsigned()->after('name');
+        });
+    }
+}
