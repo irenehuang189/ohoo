abba62053967d786e2cc0c1be14f36fed7615ff1
angelynz95 <angel_lynz95@hotmail.com>
2017-03-29 00:49:53 +0700

    fix route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -52,7 +52,7 @@ Route::group(['middleware' => 'teacher'], function() {
 	/* Score */
 	Route::get('/teacher/score', 'Teacher\ScoreController@showScores');
 	Route::get('/teacher/score/exam/add', 'Teacher\ScoreController@showAddExamForm');
-	
+	Route::post('/teacher/score/exam/add', 'Teacher\ScoreController@addExam');
 	Route::get('/teacher/score/assignment/add', 'Teacher\ScoreController@showAddAssignmentForm');
 	Route::post('/teacher/score/assignment/add', 'Teacher\ScoreController@addAssignment');
 	Route::get('/teacher/score/exam/detail/{id}', 'Teacher\ScoreController@showExamDetail');
